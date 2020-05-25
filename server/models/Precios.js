e19643f0fcfa51c06684ab61001c519a7b9ce0d1
Alexis <alexisfigueroa.dev@gmail.com>
2020-05-25 17:32:50 -0300

    Se agrega validacion en competicion y se crea schema Precios

---
+++ server/models/Precios.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+let preciosSchema = new Schema({
+
+
+
+});
+
+module.exports = mongoose.model('Precios', preciosSchema);
--- server/routes/competicion.js
+++ server/routes/competicion.js
@@ -22,26 +22,34 @@ app.post('/competicion', (req, res) => {
 
     let id = body.participante;
     let subCategoria = body.subCategoria;
+    let especialidad = body.especialidad;
+    let divisional = body.divisional;
+    let categoria = body.categoria;
 
-    Participantes.findById(id, (err, participante) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            });
-        }
 
-        if (!participante) {
-            return res.status(500).json({
-                ok: false,
-                err: {
-                    message: 'El participante no existe'
+    let valida = 0;
+
+    function validacion(valida) {
+        if (especialidad === 'ESCUELA') {
+            if (divisional === 'C' || divisional === 'B' || divisional === 'A') {
+                if (categoria !== 'INICIACION' && categoria !== 'AVANZADO') {
+                    return valida = 1;
                 }
-            });
+            }
         }
+        if (especialidad === 'LIBRE') {
+            if (divisional === 'C' || divisional === 'B' || divisional === 'A' || divisional === 'D') {
+                return valida = 2
+            }
+        }
+        return valida;
+    }
 
-        competicion.save((err, competicionDB) => {
+    validacion(valida);
+    console.log(`Resultado de la funcion ${validacion(valida)}`);
 
+    if (valida !== 0) {
+        Participantes.findById(id, (err, participante) => {
             if (err) {
                 return res.status(500).json({
                     ok: false,
@@ -49,71 +57,109 @@ app.post('/competicion', (req, res) => {
                 });
             }
 
-            if (!competicionDB) {
-                return res.status(400).json({
+            if (!participante) {
+                return res.status(500).json({
                     ok: false,
-                    err
+                    err: {
+                        message: 'El participante no existe'
+                    }
                 });
             }
 
-            res.json({
-                ok: true,
-                competicion: competicionDB
+            competicion.save((err, competicionDB) => {
 
-            });
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                }
 
-            let idCompeticion = competicionDB._id;
-            let idParticipante = competicionDB.participante;
+                if (!competicionDB) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                }
 
-            // console.log(`ID COMPETICION ${idCompeticion}`);
-            // console.log(`ID PARTICIPANTE ${idParticipante}`);
+                res.json({
+                    ok: true,
+                    competicion: competicionDB
 
-            //==================
-            // BUSCO EL ID DE PARTICIPANTE SI EXISTE EN LA COLECCION Y TRAIGO SU FECHA DE NACIMIENTO
-            //==================
+                });
 
-            Participantes.findOne({ _id: idParticipante })
-                .exec((err, participanteDB) => {
-                    if (err) {
-                        return res.status(500).json({
-                            ok: false,
-                            err
-                        });
-                    }
+                let idCompeticion = competicionDB._id;
+                let idParticipante = competicionDB.participante;
+                //==================
+                // BUSCO EL ID DE PARTICIPANTE SI EXISTE EN LA COLECCION Y TRAIGO SU FECHA DE NACIMIENTO
+                //==================
 
-                    if (!participanteDB) {
-                        return res.status(400).json({
-                            ok: false,
-                            err
+                Participantes.findOne({ _id: idParticipante })
+                    .exec((err, participanteDB) => {
+                        if (err) {
+                            return res.status(500).json({
+                                ok: false,
+                                err
+                            });
+                        }
 
-                        });
-                    }
+                        if (!participanteDB) {
+                            return res.status(400).json({
+                                ok: false,
+                                err
 
-                    //==================
-                    // Desarmar la fecha de nacimiento del participante
-                    //==================
-                    let fecha = participanteDB.fec_nac;
+                            });
+                        }
 
-                    let mes = new Date(fecha).getMonth() + 1;
-                    let dia = new Date(fecha).getDate() + 1;
-                    let año = new Date(fecha).getFullYear();
+                        //==================
+                        // Desarmar la fecha de nacimiento del participante
+                        //==================
+                        let fecha = participanteDB.fec_nac;
 
-                    let calEdad = calculoEdad(dia, mes, año);
+                        let mes = new Date(fecha).getMonth() + 1;
+                        let dia = new Date(fecha).getDate() + 1;
+                        let año = new Date(fecha).getFullYear();
 
-
-                    console.log(`Funcion calculo edad:  ${calEdad}`);
-
-
-
-                    //==================
-                    // BUSCO EL ID AGREGADO RECIENTE DE LA COMPETICION DONDE LA SUBCATEGORIA ES EDAD Y LO ACTUALIZO
-                    //==================
-                    let query = { '_id': `${idCompeticion}`, 'subCategoria': 'EDAD' };
+                        let calEdad = calculoEdad(dia, mes, año);
 
 
-                    if (subCategoria === 'EDAD' || subCategoria === undefined) {
-                        Competicion.findByIdAndUpdate(query, { $set: { subCategoria: calEdad } }, (err, participantesDB) => {
+                        console.log(`Funcion calculo edad:  ${calEdad}`);
 
+
+
+                        //==================
+                        // BUSCO EL ID AGREGADO RECIENTE DE LA COMPETICION DONDE LA SUBCATEGORIA ES EDAD Y LO ACTUALIZO
+                        //==================
+                        let query = { '_id': `${idCompeticion}`, 'subCategoria': 'EDAD' };
+
+
+                        if (subCategoria === 'EDAD' || subCategoria === undefined) {
+                            Competicion.findByIdAndUpdate(query, { $set: { subCategoria: calEdad } }, (err, participantesDB) => {
+
+                                if (err) {
+                                    return res.status(500).json({
+                                        ok: false,
+                                        err
+                                    });
+                                }
+
+                                if (!participantesDB) {
+                                    return res.status(400).json({
+                                        ok: false,
+                                        err
+                                    });
+                                }
+
+                                console.log(`Participante ${participantesDB._id} acutalizado con la edad de ${calEdad}`);
+                            });
+                        }
+
+
+                        //==================
+                        // AGREGO LA COMPETICION EN RELACION AL PARTICIPANTE
+                        //==================
+
+                        Participantes.update({ _id: idParticipante }, { $push: { competicion: idCompeticion } }, (err, participantesDB) => {
                             if (err) {
                                 return res.status(500).json({
                                     ok: false,
@@ -128,43 +174,25 @@ app.post('/competicion', (req, res) => {
                                 });
                             }
 
-                            console.log(`Participante ${participantesDB._id} acutalizado con la edad de ${calEdad}`);
-                        });
-                    }
-
-
-                    //==================
-                    // AGREGO LA COMPETICION EN RELACION AL PARTICIPANTE
-                    //==================
-
-                    Participantes.update({ _id: idParticipante }, { $push: { competicion: idCompeticion } }, (err, participantesDB) => {
-                        if (err) {
-                            return res.status(500).json({
-                                ok: false,
-                                err
-                            });
-                        }
-
-                        if (!participantesDB) {
-                            return res.status(400).json({
-                                ok: false,
-                                err
-                            });
-                        }
-
-                        console.log(`Participante ${ participanteDB._id} acutalizado`);
-                    }); // al participante buscado le agrego la nueva competicion
+                            console.log(`Participante ${ participanteDB._id} acutalizado`);
+                        }); // al participante buscado le agrego la nueva competicion
 
 
 
 
 
-                }); //findOne participante la edad
+                    }); //findOne participante la edad
 
-        }); //save Competicion
+            }); //save Competicion
 
-    }); //busco si el participante que envian es valido
+        }); //busco si el participante que envian es valido
+    } else {
+        res.status(412).json({
+            ok: false,
+            message: 'La asignacion de la competicion no corresponde a las reglas de negocio'
 
+        });
+    }
 
 });
 
