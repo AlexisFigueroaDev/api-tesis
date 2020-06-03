eab0da6522b5fcc06431d02a5808f08b119b3f82
Alexis <alexisfigueroa.dev@gmail.com>
2020-06-03 16:01:23 -0300

    Se agrega validacion del min y max de DNI

--- server/models/participantes.js
+++ server/models/participantes.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 let participantesSchema = new Schema({
     nombre_participante: { type: String, required: [true, 'El nombre es requerido'] },
     apellido_participante: { type: String, required: [true, 'El apellido es requerido'] },
-    dni_participante: { type: Number, required: [true, 'El DNI es requerido'], unique: [true, 'El DNI debe ser unico'] },
+    dni_participante: { type: Number, required: [true, 'El DNI es requerido'], min: 1000000, max: 99999999 },
     fec_nac: { type: Date, required: [true, 'La fecha de nacimiento es obligatoria \' YYYY-MM-DD \''] },
     sexo_participante: { type: Boolean, default: true },
     profesionales: { type: Schema.Types.ObjectId, ref: 'Profesionales', required: [true, 'El id del profesional es requerido'] },
